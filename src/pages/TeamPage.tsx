558240aac379591787212d9b4db424f7af24bea6
stonawski <59097648+stonawski@users.noreply.github.com>
2026-09-21 19:48:28 +0200

    Allow team hero to bleed behind navigation

--- src/pages/TeamPage.tsx
+++ src/pages/TeamPage.tsx
@@ -106,7 +106,7 @@ export function TeamPage() {
   )
 
   return (
-    <main className="overflow-hidden bg-sand-50">
+    <main className="bg-sand-50">
       <section className="relative flow-root -mt-[84px] px-5 pb-10 pt-[126px] sm:-mt-[88px] sm:pt-[136px] md:px-8 md:pb-14 md:pt-[144px]">
         <div className="pointer-events-none absolute inset-0 overflow-hidden">
           <img
